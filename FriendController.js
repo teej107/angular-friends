fb5a8cee902030b3b655727390742e9bc49fd2ce
teej107 <teej107@gmail.com>
2017-02-07 20:03:13 -0700

    Done!

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,24 @@
+angular.module('angularFriends').controller('FriendController', function ($scope)
+{
+    $scope.friends = [];
+    loadData(function (response)
+    {
+        $scope.friends = JSON.parse(response);
+        $scope.$apply();
+    });
+});
 
-
-
-
-
+function loadData(callback)
+{
+    var request = new XMLHttpRequest();
+    request.overrideMimeType('application/json');
+    request.open('GET', 'friend-data.json', true);
+    request.onreadystatechange = function ()
+    {
+        if (request.readyState == 4 && request.status == 200)
+        {
+            callback(request.responseText);
+        }
+    }
+    request.send();
+}
